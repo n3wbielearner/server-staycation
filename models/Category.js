4d39ee800c1932413eb7cbd02e35d56dc5be6f14
n3wbielearner <n3wbie.learner@gmail.com>
2021-04-24 19:54:16 +0700

    add Models/*

---
+++ models/Category.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+const { ObjectId } = mongoose.Schema;
+
+const categorySchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("Category", categorySchema);
