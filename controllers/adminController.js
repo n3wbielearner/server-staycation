2acc95cd6b989cb86642ac469363dc11ea2a6207
n3wbielearner <n3wbie.learner@gmail.com>
2021-04-25 15:12:44 +0700

    admin dashboard - handle item and booking

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -8,4 +8,10 @@ module.exports = {
   viewBank: (req, res) => {
     res.render("admin/bank/view_bank");
   },
+  viewItem: (req, res) => {
+    res.render("admin/item/view_item");
+  },
+  viewBooking: (req, res) => {
+    res.render("admin/booking/view_booking");
+  },
 };
