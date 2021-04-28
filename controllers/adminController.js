6f197b116f90d07f5a182878ce0904a4bfb88653
n3wbielearner <n3wbie.learner@gmail.com>
2021-04-28 22:27:03 +0700

    CRUD Bank - Update and Delete

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,5 +1,7 @@
 const Category = require("../models/Category");
 const Bank = require("../models/Bank");
+const fs = require("fs-extra");
+const path = require("path");
 
 module.exports = {
   viewDashboard: (req, res) => {
@@ -129,7 +131,50 @@ module.exports = {
       res.redirect("/admin/bank");
     }
   },
-
+  editBank: async (req, res) => {
+    try {
+      const { id, name, nameBank, nomorRekening } = req.body;
+      const bank = await Bank.findOne({ _id: id });
+      if (req.file == undefined) {
+        bank.name = name;
+        bank.nameBank = nameBank;
+        bank.nomorRekening = nomorRekening;
+        await bank.save();
+        req.flash("alertMessage", "Success Update Bank");
+        req.flash("alertStatus", "success");
+        res.redirect("/admin/bank");
+      } else {
+        await fs.unlink(path.join(`public/${bank.imageUrl}`));
+        bank.name = name;
+        bank.nameBank = nameBank;
+        bank.nomorRekening = nomorRekening;
+        bank.imageUrl = `images/${req.file.filename}`;
+        await bank.save();
+        req.flash("alertMessage", "Success Update Bank");
+        req.flash("alertStatus", "success");
+        res.redirect("/admin/bank");
+      }
+    } catch (error) {
+      req.flash("alertMessage", `${error.message}`);
+      req.flash("alertStatus", "danger");
+      res.redirect("/admin/bank");
+    }
+  },
+  deleteBank: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const bank = await Bank.findOne({ _id: id });
+      await fs.unlink(path.join(`public/${bank.imageUrl}`));
+      await bank.remove();
+      req.flash("alertMessage", "Success Delete Bank");
+      req.flash("alertStatus", "success");
+      res.redirect("/admin/bank");
+    } catch (error) {
+      req.flash("alertMessage", `${error.message}`);
+      req.flash("alertStatus", "danger");
+      res.redirect("/admin/bank");
+    }
+  },
   viewItem: (req, res) => {
     res.render("admin/item/view_item", {
       title: "Staycation | Item",
--- routes/admin.js
+++ routes/admin.js
@@ -3,13 +3,23 @@ const adminController = require("../controllers/adminController");
 const { upload } = require("../middlewares/multer");
 
 router.get("/dashboard", adminController.viewDashboard);
+
+//endpoint category
 router.get("/category", adminController.viewCategory);
 router.post("/category", adminController.addCategory);
 router.put("/category", adminController.editCategory);
 router.delete("/category/:id", adminController.deleteCategory);
+
+//endpoint bank
 router.get("/bank", adminController.viewBank);
 router.post("/bank", upload, adminController.addBank);
+router.put("/bank", upload, adminController.editBank);
+router.delete("/bank/:id", adminController.deleteBank);
+
+//endpoint item
 router.get("/item", adminController.viewItem);
+
+//endpoint booking
 router.get("/booking", adminController.viewBooking);
 
 module.exports = router;
